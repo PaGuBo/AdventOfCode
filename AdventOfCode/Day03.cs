70cf4e512cbbd8597380158499ec5f7ab9c07c78
Pavel <pavelgutin@gmail.com>
2016-12-05 13:57:32 -0500

    day 3, part 1

---
+++ AdventOfCode/Day03.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day03
+    {
+        public static int NumberOfValidTriangles(int[,] triangles)
+        {
+            int valid = 0;
+            for (int i = 0; i < triangles.GetLength(0); i++)
+            {
+                if ((triangles[i, 0] + triangles[i, 1] > triangles[i, 2]) &&
+                    (triangles[i, 0] + triangles[i,2] > triangles[i, 1]) &&
+                    (triangles[i, 1] + triangles[i, 2] > triangles[i, 0]) )
+                {
+                    valid++;
+                }
+            }
+            return valid;
+        }
+    }
+}
