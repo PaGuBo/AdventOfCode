30884dee9c85aec095658070153f815649609ba0
Pavel <pavelgutin@gmail.com>
2016-12-09 17:32:38 -0500

    broken commit, no time to finish this now

---
+++ Puzzles/Day05.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzles
+{
+    public static class Day05
+    {
+        public static int CountNiceString(string input)
+        {
+            int niceStrings = 0;
+            var strings = input.Split('\n');
+            var vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            var forbidden = new List<string> { "ab", "cd", "pq", "xy" };
+            foreach(var str in strings)
+            {
+                if (str.Where(c => vowels.Contains(c)).Distinct().Count() >= 3 &&
+                    str.Substring(0, str.Length - 1).Where((c, i) => str[i] == str[i + 1]).Count() > 0 &&
+                    !str.Any(
+                    )
+                {
+                    
+                }
+            }
+            return niceStrings;
+        }
+    }
+}
---
+++ PuzzlesTests/Day05Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Puzzles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzles.Tests
+{
+    [TestClass()]
+    public class Day05Tests
+    {
+        [TestMethod()]
+        [DeploymentItem(@"Inputs\", "Inputs")]
+        public void CountNiceStringTest()
+        {
+            string testData = System.IO.File.ReadAllText(@"Inputs\naughtyNiceStrings.txt").Replace("\r", "");
+            Assert.AreEqual(2, Day05.CountNiceString(testData));
+        }
+
+    }
+}
