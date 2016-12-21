89e7410211479ee8d6bbb392b5974b9ed7c5b8c0
Pavel <pavelgutin@gmail.com>
2016-12-21 13:50:08 -0500

    Breadth first seems to be a lot easier to deal with without recursion, at least in this case.

---
+++ AdventOfCode.Test/Day17Tests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace AdventOfCode.Tests
+{
+    [TestClass()]
+    public class Day17Tests
+    {
+        [TestMethod()]
+        public void SolveTest_0()
+        {
+            Assert.AreEqual("No path exists", Day17.GetShortestPath("hijkl", new Point(0, 0), new Point(3, 3)));
+        }
+        
+        [TestMethod()]
+        public void SolveTest_1()
+        {
+            Assert.AreEqual("DDRRRD", Day17.GetShortestPath("ihgpwlah", new Point(0, 0), new Point(3, 3)));
+        }
+
+        [TestMethod()]
+        public void SolveTest_2()
+        {
+            Assert.AreEqual("DDUDRLRRUDRD", Day17.GetShortestPath("kglvqrro", new Point(0, 0), new Point(3, 3)));
+        }
+
+        [TestMethod()]
+        public void SolveTest_3()
+        {
+            Assert.AreEqual("DRURDRUDDLLDLUURRDULRLDUUDDDRR", Day17.GetShortestPath("ulqzkmiv", new Point(0, 0), new Point(3, 3)));
+        }
+
+        [TestMethod()]
+        public void SolveTest_4()
+        {
+            Assert.AreEqual("DRRDRLDURD", Day17.GetShortestPath("pvhmgsws", new Point(0, 0), new Point(3, 3)));
+        }
+
+        [TestMethod()]
+        public void LongestPathTest_1()
+        {
+            Assert.AreEqual(370, Day17.GetLongestPathLength("ihgpwlah", new Point(0, 0), new Point(3, 3)));
+        }
+
+        [TestMethod()]
+        public void LongestPathTest_2()
+        {
+            Assert.AreEqual(492, Day17.GetLongestPathLength("kglvqrro", new Point(0, 0), new Point(3, 3)));
+        }
+
+        [TestMethod()]
+        public void LongestPathTest_3()
+        {
+            Assert.AreEqual(830, Day17.GetLongestPathLength("ulqzkmiv", new Point(0, 0), new Point(3, 3)));
+        }
+
+        [TestMethod()]
+        public void LongestPathTest_4()
+        {
+            Assert.AreEqual(830, Day17.GetLongestPathLength("pvhmgsws", new Point(0, 0), new Point(3, 3)));
+        }
+    }
+}
---
+++ AdventOfCode/Day17.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Day17
+    {
+        public static string GetShortestPath(string password, Point startPos, Point goalPos)
+        {
+            var q = new Queue<Tuple<string, Point>>();
+            q.Enqueue(new Tuple<string, Point>("", startPos));
+
+            bool up = true;
+            bool down = true;
+            bool left = true;
+            bool right = true;
+
+            while (q.Count > 0)
+            {
+                var item = q.Dequeue();
+                var currentPath = item.Item1;
+                var currPos = item.Item2;
+
+                if (currPos == goalPos)
+                {
+                    return currentPath;
+                }
+                else
+                {
+                    CheckDoors(password + currentPath, out up, out down, out left, out right);
+                    if (!up && !down && !left && !right)
+                    {
+                        continue;
+                    }
+                    if (up && currPos.Y > 0) //we can go up and it's open
+                    {
+                        q.Enqueue(new Tuple<string, Point>(currentPath + "U", new Point(currPos.X, currPos.Y - 1)));
+                    }
+                    if (down && currPos.Y < 3) //we can go down and it's open
+                    {
+                        q.Enqueue(new Tuple<string, Point>(currentPath + "D", new Point(currPos.X, currPos.Y + 1)));
+                    }
+                    if (left && currPos.X > 0) //we can go left and it's open
+                    {
+                        q.Enqueue(new Tuple<string, Point>(currentPath + "L", new Point(currPos.X - 1, currPos.Y)));
+                    }
+                    if (right && currPos.X < 3) //we can go right and it's open
+                    {
+                        q.Enqueue(new Tuple<string, Point>(currentPath + "R", new Point(currPos.X + 1, currPos.Y)));
+                    }
+                }
+            }
+            return "No path exists";
+        }
+
+
+        public static int GetLongestPathLength(string password, Point startPos, Point goalPos)
+        {
+            var longestPathLength = 0;
+            var q = new Queue<Tuple<string, Point>>();
+            q.Enqueue(new Tuple<string, Point>("", startPos));
+
+            bool up = true;
+            bool down = true;
+            bool left = true;
+            bool right = true;
+
+            while (q.Count > 0)
+            {
+                var item = q.Dequeue();
+                var currentPath = item.Item1;
+                var currPos = item.Item2;
+
+                if (currPos == goalPos)
+                {
+                    if (currentPath.Length > longestPathLength)
+                    {
+                        longestPathLength = currentPath.Length;
+                    }
+                }
+                else
+                {
+                    CheckDoors(password + currentPath, out up, out down, out left, out right);
+                    if (!up && !down && !left && !right)
+                    {
+                        continue;
+                    }
+                    if (up && currPos.Y > 0) //we can go up and it's open
+                    {
+                        q.Enqueue(new Tuple<string, Point>(currentPath + "U", new Point(currPos.X, currPos.Y - 1)));
+                    }
+                    if (down && currPos.Y < 3) //we can go down and it's open
+                    {
+                        q.Enqueue(new Tuple<string, Point>(currentPath + "D", new Point(currPos.X, currPos.Y + 1)));
+                    }
+                    if (left && currPos.X > 0) //we can go left and it's open
+                    {
+                        q.Enqueue(new Tuple<string, Point>(currentPath + "L", new Point(currPos.X - 1, currPos.Y)));
+                    }
+                    if (right && currPos.X < 3) //we can go right and it's open
+                    {
+                        q.Enqueue(new Tuple<string, Point>(currentPath + "R", new Point(currPos.X + 1, currPos.Y)));
+                    }
+                }
+            }
+            return longestPathLength;
+        }
+        private static void CheckDoors(string input, out bool up, out bool down, out bool left, out bool right)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var openSymbols = new List<char> { 'B', 'C', 'D', 'E', 'F' };
+                var hashArray = md5.ComputeHash(Encoding.ASCII.GetBytes(input));
+                var hashString = BitConverter.ToString(hashArray).Replace("-", "");
+
+                up = openSymbols.Contains(hashString[0]);
+                down = openSymbols.Contains(hashString[1]);
+                left = openSymbols.Contains(hashString[2]);
+                right = openSymbols.Contains(hashString[3]);
+            }
+        }
+    }
+}
