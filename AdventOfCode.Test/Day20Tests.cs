8998ad3a0683b1cd4e44a6f590fef03493ad38eb
Pavel <pavelgutin@gmail.com>
2016-12-22 08:45:26 -0500

    issues

---
+++ AdventOfCode.Test/Day20Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.SleighPuzzles.Tests
+{
+    [TestClass()]
+    public class Day20Tests
+    {
+        [TestMethod()]
+        [DeploymentItem(@"2016\Inputs\", "Inputs")]
+        public void LowestAllowedIpTest()
+        {
+            var testData = System.IO.File.ReadAllLines(@"Inputs\Day20simple.txt");
+            
+            Assert.AreEqual(3, Day20.LowestAllowedIp(testData));
+        }
+    }
+}
---
+++ AdventOfCode/Day20.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Day20
+    {
+        public class Range
+        {
+            public int Min;
+            public int Max;
+        }
+        public static int LowestAllowedIp(string[] ranges)
+        {
+            int lowest = 0;
+            var cleanRanges = new List<Range>();
+            foreach (var range in ranges)
+            {
+                var s = range.Split('-');
+                var low = Convert.ToInt32(range[0]);
+                var high = Convert.ToInt32(range[1]);
+                var newRange = new Range() { Min = low, Max = high };
+
+                //check if any ranges fall totally inside new range. They can be removed
+                var rangesInsideNewRange = cleanRanges.Where(x => x.Min >= low && x.Max <= high);
+                if (rangesInsideNewRange.Count() > 0)
+                {
+                    foreach (var r in rangesInsideNewRange)
+                    {
+                        cleanRanges.Remove(r);
+                    }
+                    cleanRanges.Add(newRange);
+                }
+                ////check if the end of the range falls inside another range
+                //var rangesContainingLow = cleanRanges.Where(x => x.Min < low && x.Max > low);
+                //if (rangesContainingLow.Count() > 0)
+                //{
+                //    foreach (var r in rangesContainingLow)
+                //    {
+                //        cleanRanges.Remove(r);
+                //        newRange.Max = r.Max;
+                //        cleanRanges.Add(newRange);
+
+                //    }
+                //}
+                //else
+                //{
+                    
+                //}
+            }
+            return lowest;
+        }
+    }
+}
